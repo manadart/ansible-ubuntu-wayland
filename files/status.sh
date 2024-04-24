eeda1a31fbe4cbf6fd60ba76f3bc9d21ab6c6399
Joseph Phillips <joe@manalabs.eu>
2024-04-24 19:43:57 +0200

    Updates swaybar status with load average and swap percentage.

--- files/status.sh
+++ files/status.sh
@@ -1,4 +1,8 @@
+loadavg_1m=$(cat /proc/loadavg | awk -F ' ' '{print $1}')
+
 mem_percent=$(free -t | awk 'FNR == 2 {printf("%.0f%"), $3/$2*100}')
+
+swap_percent=$(free -t | awk 'FNR == 3 {printf("%.0f%"), $3/$2*100}')
 
 batt_all=$(upower -i $(upower -e | grep BAT))
 
@@ -8,8 +12,8 @@ batt_status=$(echo "$batt_all" | grep "state" | awk '{print $2}')
 
 batt_time=$(echo "$batt_all" | grep "time to" | awk '{$1=$2=$3=""; print $0}' | xargs)
 
-kernel_version=$(uname -r | cut -d '-' -f1)
+kernel_ver=$(uname -r | cut -d '-' -f1)
 
-date_formatted=$(date "+%a %F %H:%M")
+date_fmt=$(date "+%a %F %H:%M")
 
-echo "$mem_percent | $batt_percent - $batt_status - $batt_time | $kernel_version | $date_formatted"
+echo "$loadavg_1m | $mem_percent * $swap_percent | $batt_percent - $batt_status - $batt_time | $kernel_ver | $date_fmt"
